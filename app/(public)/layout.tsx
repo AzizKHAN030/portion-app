678c713af106ca5ba743968a384dd3c71b5b9f5f
Azizbek Rasulov <azizbek.rasulov@scandiweb.com>
2023-11-19 00:27:02 +0500

    Dark mode fix

--- app/(public)/layout.tsx
+++ app/(public)/layout.tsx
@@ -1,6 +1,6 @@
 const PublicLayout = ({ children }: { children: React.ReactNode }) => {
   return (
-    <main className="flex-1 h-full overflow-y-auto bg-[#1F1F1F]">
+    <main className="flex-1 h-full overflow-y-auto dark:bg-[#1F1F1F]">
       {children}
     </main>
   );
